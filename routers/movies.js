e6fb65de88abe6b70f96abb9e7825ae593edf963
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-03-14 18:24:35 +0100

    [add]new route - store

--- routers/movies.js
+++ routers/movies.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const upload = require('../middlewares/multer');
+
 const movieController = require("../controllers/moviesController");
 
 router.get('/', movieController.index);
 router.get('/:id', movieController.show);
 router.post('/:id/reviews', movieController.storeReview);
+router.post('/', upload.single('image'),  movieController.store);
 
 
 module.exports = router;
